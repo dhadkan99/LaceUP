e7b025429a668c63b1b324d340c01941ff164199
Dhadkan <dhadkankc88@gmail.com>
2025-06-26 13:43:33 +0545

    added shopping page

--- forntend/src/components/Header.jsx
+++ forntend/src/components/Header.jsx
@@ -4,14 +4,14 @@ import { MdOutlineLanguage } from "react-icons/md";
 
 function Header() {
   return (
-    <header className="fixed top-0 left-0 z-50 w-full bg-transparent shadow-sm">
+    <header className="fixed top-0 left-0 z-50 w-full bg-gray-100 shadow-sm">
       <nav className="container flex justify-center items-center px-6 py-4 mx-auto">
         <div className="absolute left-6">
           <Link to="/">
-            <h1 className="flex text-4xl font-extrabold text-white">Lace UP</h1>
+            <h1 className="flex text-4xl font-extrabold text-black">Lace UP</h1>
           </Link>
         </div>
-        <ul className="flex justify-center space-x-8 font-medium text-center text-white">
+        <ul className="flex justify-center space-x-8 font-medium text-center text-black">
           <li>
             <Link to="/" className="hover:text-gray-600">
               Home
@@ -43,12 +43,12 @@ function Header() {
             </Link>
           </li>
         </ul>
-        <div className="flex absolute right-6 items-center space-x-4 text-xl text-white">
+        <div className="flex absolute right-6 items-center space-x-4 text-xl text-black">
           <button title="Language" className="hover:text-gray-600">
             <MdOutlineLanguage />
           </button>
-          <button title="Profile" className="hover:text-gray-600">
-            👤
+          <button title="Sign in" className="hover:text-gray-600">
+            Sign IN
           </button>
         </div>
       </nav>
